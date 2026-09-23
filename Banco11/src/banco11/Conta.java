3482da63d9966e39423e909a60f975d9b1dc7e13
Augusto Domingues <augusto070509@gmail.com>
2026-09-23 17:48:40 -0300

    AUGUSTO

---
+++ Banco11/src/banco11/Conta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco11;
+
+/**
+ *
+ * @author augusto62977786
+ */
+public class conta {
+    
+}
